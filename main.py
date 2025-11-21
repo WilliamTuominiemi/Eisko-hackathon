41733b0996f74a3cc874e9cbebf2139c0efd861f
Tuominiemi William <william.tuominiemi@aalto.ficd>
2025-11-21 23:13:42 +0200

    game plan

---
+++ main.py
@@ -0,0 +1,12 @@
+# Read pdf into folder
+ 
+# For each image
+
+## Extract cells from all images
+## Extract suojas from all images
+## Check that these are same length
+## Append suojas to result array
+
+# Counting
+
+# Display end result in html
